5142299452bc2debcb00875e26c3b7119ea67dca
Michael Ring <ringmrr@gmail.com>
2021-01-13 01:18:40 -0800

    Update gan.py
    Added the rest of code from Unconventional Neural Networks p.3

--- gan.py
+++ gan.py
@@ -1,7 +1,10 @@
 # Imports the function needed to import data.
 from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as pyplot
+import numpy as numpy
 
 # Imports the MNIST training set.
+# The argument one_hot=True is a way of encoding categorical data as numerical data. Read more here: https://machinelearningmastery.com/why-one-hot-encode-data-in-machine-learning/
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # Shuffles the data set, and returns 100 random samples.
@@ -11,8 +14,11 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 batch_xs, batch_ys = mnist.train.next_batch(100)
 
 # Prints the features and label for the first sample.
-data = batch_xs[0]
-label = batch_ys[0]
+data = np.rint(batch_xs[0]).astype(int)
+label = np.rint(batch_ys[0]).astype(int)
+pixels = data.reshape((28,28))
 
 print(data)
 print(label)
+plt.imshow(pixels, cmap="gray")
+plt.show()
